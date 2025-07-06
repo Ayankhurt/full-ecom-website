9c2aa274a29e4187365e83cc469bb19320dea673
Ayan Khurt <ayankhurt@gmail.com>
2025-07-07 01:21:09 +0500

    new

--- ecom-front/src/main.jsx
+++ ecom-front/src/main.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import ContextProvider from './context/Context.jsx';
+import axios from 'axios';
 
+// Set axios to send cookies with requests
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  <ContextProvider>
-    <App />
-  </ContextProvider>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </BrowserRouter>
 );

4a1185cb23415d053ac5d66e0d8219efc1d9fb5c
Ayan Khurt <ayankhurt@gmail.com>
2025-07-10 23:36:42 +0500

    new

--- ecom-front/src/api.js
+++ ecom-front/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://full-ecom-website-nu.vercel.app",
+  baseURL: "https://full-ecom-website-nu.vercel.app/api/v1/",
   withCredentials: true,
 });
 export default api;

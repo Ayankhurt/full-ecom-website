f7078e6eacdbecf2d9600d8d115601653852d14d
Ayan Khurt <ayankhurt@gmail.com>
2025-07-12 02:24:03 +0500

    nwe

--- ecom-front/src/App.jsx
+++ ecom-front/src/App.jsx
@@ -1,17 +1,25 @@
 import './App.css';
 import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom';
 import Signup from './pages/signup';
 import Login from './pages/login';
 import Category from './pages/category';
 import AddProduct from './pages/add-product';
 import Home from './pages/home';
 import Navbar from './pages/Navbar';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { GlobalContext } from './context/Context.jsx';
 
 function App() {
   let {state, dispatch} = useContext(GlobalContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (window.location.pathname === '/home/') {
+      navigate('/home', { replace: true });
+    }
+  }, [navigate]);
+
   return (
     <div>
       <Navbar />
